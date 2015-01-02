59b5c4e1841a570dfb7c8026505880ce98e76e83
Guido Aulisi <guido.aulisi@gmail.com>
2015-01-02 20:24:15 +0100

    Correct license

--- main.cc
+++ main.cc
@@ -2,12 +2,12 @@
  * tombola
  * Copyright (C) 2014 Guido Aulisi <guido.aulisi@gmail.com>
  *
- * pg800 is free software: you can redistribute it and/or modify it
+ * tombola is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * pg800 is distributed in the hope that it will be useful, but
+ * tombola is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
