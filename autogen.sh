7a9b8f11cd12ed1a9f5ae4acdad0170ad494d4e7
Guido Aulisi <guido.aulisi@gmail.com>
2024-01-05 15:18:20 +0100

    Autotools upgrade

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 mkdir -p icons/{48,128}
-inkscape --export-png=icons/128/tombola.png icons/tombola-128x128.svg
-inkscape --export-png=icons/48/tombola.png icons/tombola-48x48.svg
+inkscape --export-type=png --export-filename=icons/128/tombola.png icons/tombola-128x128.svg
+inkscape --export-type=png --export-filename=icons/48/tombola.png icons/tombola-48x48.svg
 autoreconf -if
